caf3e0e0533ddb3263c5a338ab1671231db9cac4
Elias Neto <netoeac.dev@gmail.com>
2024-04-18 23:58:08 -0300

    update isbn type

--- src/models/book.js
+++ src/models/book.js
@@ -4,7 +4,7 @@ const BookSchema = new mongoose.Schema({
     id: { type: Number, require: true },
     title: { type: String, required: true },
     pageNumbers: { type: Number, required: true },
-    isbn: { type: Number, required: true, unique: true },
+    isbn: { type: String, required: true, unique: true },
     publishCompany: { type: String, required: true }
 })
 export default mongoose.models.Book || mongoose.model('Book', BookSchema)
