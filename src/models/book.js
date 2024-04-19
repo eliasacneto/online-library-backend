f83d62a2ce6f326cab95415ef7dc95e63277b0bb
Elias Neto <netoeac.dev@gmail.com>
2024-04-18 23:58:51 -0300

    update

--- src/models/book.js
+++ src/models/book.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const BookSchema = new mongoose.Schema({
-    id: { type: Number, require: true },
+    id: { type: Number, require: true, unique: true },
     title: { type: String, required: true },
     pageNumbers: { type: Number, required: true },
-    isbn: { type: String, required: true, unique: true },
+    isbn: { type: String, required: true },
     publishCompany: { type: String, required: true }
 })
 export default mongoose.models.Book || mongoose.model('Book', BookSchema)
