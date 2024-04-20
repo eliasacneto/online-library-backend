52b40fd5b79ee682917bd80ab336f56aa35ab12b
Elias Neto <netoeac.dev@gmail.com>
2024-04-20 13:18:33 -0300

    update

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import 'dotenv/config'
 
-const URI = process.env.DB_KEY
+// const URI = process.env.DB_KEY
+const URI = 'mongodb+srv://admin:xinYSL9HGcAOaJcV@cluster0.wcdaozq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const databaseConnection = async () => {
 
