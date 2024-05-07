0f40481b336cc4ae7eddebd80d6505bb77cc8956
Elias Neto <netoeac.dev@gmail.com>
2024-05-07 00:31:37 -0300

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import userController from './controllers/book.js'
 import booksController from './controllers/books.js'
 import cors from 'cors'
 import book from './models/book.js'
+import { listBooks } from './services/book.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001;
@@ -11,17 +12,15 @@ app.use(cors())
 
 app.use(bodyParser.json())
 
-app.get('/', async (req, res) => {
-    res.send('🔥 Library Backend is on!')
-})
+// app.get('/', async (req, res) => {
+//     res.send('🔥 Library Backend is on!')
+// })
 
 app.use('/book', userController)
-app.use('/books', booksController)
 
-app.get('/books', (req, res) => {
-    Book.find()
-        .then((books) => res.json(books))
-        .catch((err) => res.status(400).json({ error: err.message }))
+app.get('/', async (req, res) => {
+    const bookList = await listBooks()
+    res.send(bookList)
 })
 
 app.listen(PORT, () => { console.log(` 🔥 Server is running...`) })
